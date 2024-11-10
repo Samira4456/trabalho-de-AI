3d582ea32aed71f48fcea25132b17855c377ad3e
Samira4456 <ssamiralourrany@gmail.com>
2024-11-10 19:23:12 +0000

    Create Calculadora.py
    funções da calculadora

---
+++ Calculadora.py
@@ -0,0 +1,20 @@
+#calculadora
+
+#Funçao para soma
+def adicionar(x, y):
+    return x + y
+
+#Funçao para subtraçao
+def subtrair(x, y):
+    return x - y
+
+#Funçao para multiplicaçao
+def multiplicar(x, y):
+    return x * y
+
+#Funçao para dividir
+def dividir(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro! Divisão por zero."
